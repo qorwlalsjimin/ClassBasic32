b01e25106db813ba79dc2eda28d55a97b7c496eb
Jimin Baek <qorwlalsjimin@naver.com>
2023-05-08 12:16:53 +0900

    class 이름이 중복되는 경우

--- ClassBasic32/Program.cs
+++ ClassBasic32/Program.cs
@@ -4,8 +4,16 @@ namespace NewClassProgram3206 // Note: actual namespace depends on the project n
 {
     internal class Program
     {
+        private class Math { 
+            static public int Abs(int a)
+            {
+                return 0;
+            }
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine(Math.Abs(-10));
             ThirdClass abc;
             ABCClass abc2;
             InnerClass inn;
